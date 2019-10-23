17acaa9f93a888459efd81baecb93433a5eead92
tscz <teje.sczeponek@gmx.de>
2019-10-23 17:29:13 +0900

    Add Sidebar and File Menue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -7,8 +8,8 @@ import DrumPage from "./pages/DrumPage";
 import HarmonyPage from "./pages/HarmonyPage";
 import ImportPage from "./pages/ImportPage";
 import PrintPage from "./pages/PrintPage";
-import StrummingPage from "./pages/StrummingPage";
 import SongStructurePage from "./pages/SongStructurePage";
+import StrummingPage from "./pages/StrummingPage";
 
 class App extends React.Component {
   switchSong = (file: string) => {
@@ -34,62 +35,113 @@ class App extends React.Component {
     console.log("render App with src=" + this.state.src);
 
     return (
-      <div className="App">
-        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-          <Navbar.Brand>Transcribe</Navbar.Brand>
+      <>
+        <Navbar
+          bg="dark"
+          variant="dark"
+          expand="lg"
+          sticky="top"
+          className="navbar navbar-expand-lg navbar-light bg-light border-bottom"
+        >
+          <Navbar.Brand
+            onClick={(event: any) => {
+              document!.querySelector("#wrapper")!.classList.toggle("toggled");
+            }}
+          >
+            Transcribe
+          </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <NavDropdown title="File" id="basic-nav-dropdown">
+              <NavDropdown.Item
+                onClick={() =>
+                  this.switchPage(<SongStructurePage file={this.state.src} />)
+                }
+              >
+                New
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item
+                onClick={() => this.switchPage(<HarmonyPage />)}
+              >
+                Open ...
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item
+                onClick={() => this.switchPage(<HarmonyPage />)}
+              >
+                Save
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Preferences" id="basic-nav-dropdown">
+              <NavDropdown.Item onClick={() => this.switchPage(<DrumPage />)}>
+                Preferences...
+              </NavDropdown.Item>
+            </NavDropdown>
             <Nav className="mr-auto">
               <Nav.Link
                 onClick={() =>
                   this.switchPage(<ImportPage callback={this.switchSong} />)
                 }
               >
-                Home
-              </Nav.Link>
-              <Nav.Link
-                onClick={() =>
-                  this.switchPage(<ImportPage callback={this.switchSong} />)
-                }
-              >
-                Import Song
-              </Nav.Link>
-              <NavDropdown title="Analyse Song" id="basic-nav-dropdown">
-                <NavDropdown.Item
-                  onClick={() =>
-                    this.switchPage(<SongStructurePage file={this.state.src} />)
-                  }
-                >
-                  Song Structure
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  onClick={() => this.switchPage(<HarmonyPage />)}
-                >
-                  Harmony
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => this.switchPage(<DrumPage />)}>
-                  Drum Pattern
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  onClick={() => this.switchPage(<StrummingPage />)}
-                >
-                  Strumming Pattern
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link onClick={() => this.switchPage(<PrintPage />)}>
-                Print Song
+                Help
               </Nav.Link>
             </Nav>
-            <audio id="audio" controls>
-              {this.state.src !== "" && (
-                <source src={this.state.src} type="audio/mpeg" />
-              )}
-              Your browser does not support the audio element.
-            </audio>
           </Navbar.Collapse>
         </Navbar>
-        {this.state.currentPage}
-      </div>
+
+        <div className="d-flex" id="wrapper">
+          <div className="bg-light border-right" id="sidebar-wrapper">
+            <ListGroup variant="flush">
+              <ListGroup.Item
+                action
+                onClick={() =>
+                  this.switchPage(<SongStructurePage file={this.state.src} />)
+                }
+              >
+                Song Structure
+              </ListGroup.Item>
+              <ListGroup.Item
+                action
+                onClick={() => this.switchPage(<HarmonyPage />)}
+              >
+                Harmony
+              </ListGroup.Item>
+              <ListGroup.Item
+                action
+                onClick={() => this.switchPage(<DrumPage />)}
+              >
+                Drum Pattern
+              </ListGroup.Item>
+              <ListGroup.Item
+                action
+                onClick={() => this.switchPage(<StrummingPage />)}
+              >
+                Strumming Pattern
+              </ListGroup.Item>
+              <ListGroup.Item
+                action
+                onClick={() => this.switchPage(<PrintPage />)}
+              >
+                Print Song
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <audio id="audio" controls>
+                  {this.state.src !== "" && (
+                    <source src={this.state.src} type="audio/mpeg" />
+                  )}
+                  Your browser does not support the audio element.
+                </audio>
+              </ListGroup.Item>
+            </ListGroup>
+          </div>
+
+          <div id="page-content-wrapper">
+            <div className="container-fluid">{this.state.currentPage}</div>
+          </div>
+        </div>
+      </>
     );
   }
 }
