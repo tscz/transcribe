8e7b668d7a983802fbf84ec6409ef3e2f11c16ff
tscz <tscz@users.noreply.github.com>
2019-12-15 18:07:47 +0900

    #14 Add git and npm version information during build
    Use the https://create-react-app.dev/docs/adding-custom-environment-variables/
    and cross.env)

--- src/common/versionInfo.tsx
+++ src/common/versionInfo.tsx
@@ -1,60 +1,18 @@
 import React from "react";
 
-class VersionInfo extends React.Component<
-  {},
-  { appVersion: string; hash: string; description: string }
-> {
-  constructor(props: any) {
-    super(props);
-    this.state = { appVersion: "", hash: "", description: "" };
-  }
-
-  componentDidMount() {
-    this.setStateFromFile({
-      file: "/gitInfo.txt",
-      regex: /([0-9a-f]{7}) (.*)/,
-      stateObject: regexResult => {
-        return { hash: regexResult[1], description: regexResult[2] };
-      }
-    });
-
-    this.setStateFromFile({
-      file: "/npmInfo.txt",
-      regex: /[0-9]+.[0-9]+.[0-9]+/,
-      stateObject: regexResult => {
-        return { appVersion: regexResult[0] };
-      }
-    });
-  }
-
-  setStateFromFile({
-    file,
-    regex,
-    stateObject
-  }: {
-    file: string;
-    regex: RegExp;
-    stateObject: (regexResult: RegExpExecArray) => object;
-  }) {
-    fetch(process.env.PUBLIC_URL + file)
-      .then(response => response.text())
-      .then(text => {
-        let extraction = regex.exec(text);
-
-        if (extraction !== null && extraction[0] !== null) {
-          this.setState(stateObject(extraction));
-        }
-      });
-  }
-
+class VersionInfo extends React.Component {
   render() {
     return (
       <>
-        {this.state.appVersion} (
+        {process.env.REACT_APP_VERSION} (
         <a
-          href={"https://github.com/tscz/transcribe/commit/" + this.state.hash}
+          href={
+            "https://github.com/tscz/transcribe/commit/" +
+            process.env.REACT_APP_VERSION_HASH
+          }
         >
-          {this.state.hash} - {this.state.description}
+          {process.env.REACT_APP_VERSION_HASH} -{" "}
+          {process.env.REACT_APP_VERSION_DESCRIPTION}
         </a>
         )
       </>
