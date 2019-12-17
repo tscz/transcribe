9cfd5aba1cd1ce7ef15b83840001f6995e68de2a
tscz <teje.sczeponek@gmx.de>
2019-12-17 09:29:23 +0900

    #6 Remove properties view from default page

--- src/pages/default/defaultPage.tsx
+++ src/pages/default/defaultPage.tsx
@@ -1,26 +1,14 @@
 import React from "react";
 
-import ContentLayout from "../../components/contentLayout/contentLayout";
 import View from "../../components/view/view";
-import WaveControlView from "../../views/waveControl/waveControlView";
 
 class DefaultPage extends React.Component {
   render() {
     return (
-      <ContentLayout
-        topLeft={
-          <View
-            header="No Analysis started"
-            body={<p>Please create a new Analysis or open an existing one.</p>}
-          ></View>
-        }
-        topRight={
-          <View
-            header="Properties"
-            body={<WaveControlView></WaveControlView>}
-          ></View>
-        }
-      ></ContentLayout>
+      <View
+        header="No Transcription started"
+        body={<p>Please create a new Transcription or open an existing one.</p>}
+      ></View>
     );
   }
 }
