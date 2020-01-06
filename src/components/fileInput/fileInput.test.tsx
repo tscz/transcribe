332cd444e1886a365ee6d8be4fe712604736b5ed
tscz <tscz@users.noreply.github.com>
2020-01-07 01:40:04 +0900

    #6 Add tests for file chooser

--- src/components/fileInput/fileInput.test.tsx
+++ src/components/fileInput/fileInput.test.tsx
@@ -1,3 +1,4 @@
+import { IconButton, InputBase } from "@material-ui/core";
 import { mount } from "enzyme";
 import React from "react";
 
@@ -41,6 +42,88 @@ it("can select a file from local disk", () => {
   expect(mockCallback).toHaveBeenCalledTimes(1);
   expect(mockCallback.mock.calls[0][0].name).toEqual(mockFile.name);
   expect(mockCallback.mock.calls[0][1]).toEqual(mockUrl);
+});
+
+it("triggers the os file input dialog on icon button and text input click", () => {
+  //Render test component
+  const wrapper = mount(
+    <FileInput id="zipFileInput" fileType={FileType.ZIP} callback={() => {}} />
+  );
+  const inputClickMock = jest.fn();
+  wrapper
+    .find("input#zipFileInput")
+    .getDOMNode<HTMLInputElement>().onclick = inputClickMock;
+
+  // Click on the icon button
+  wrapper.find(IconButton).simulate("click", {
+    target: {
+      files: null
+    }
+  });
+  expect(inputClickMock).toHaveBeenCalledTimes(1);
+
+  // Click on the text input
+  inputClickMock.mockReset();
+  wrapper.find(InputBase).simulate("click", {
+    target: {
+      files: null
+    }
+  });
+  expect(inputClickMock).toHaveBeenCalledTimes(1);
+});
+
+it("does not select a file if file chooser dialog is canceled", () => {
+  const mockCallback = jest.fn((file: File, fileURL: string) => {});
+
+  //Render test component
+  const wrapper = mount(
+    <FileInput
+      id="zipFileInput"
+      fileType={FileType.ZIP}
+      callback={mockCallback}
+    />
+  );
+
+  // Simulate canceling by sending event with files=null
+  wrapper.find("input#zipFileInput").simulate("change", {
+    target: {
+      files: null
+    }
+  });
+
+  expect(mockCallback).not.toBeCalled();
+});
+
+it("can be filtered for audio file types", () => {
+  const wrapper = mount(
+    <FileInput
+      id="audioFileInput"
+      fileType={FileType.AUDIO}
+      callback={(file: File, fileURL: string) => {}}
+    />
+  );
+
+  expect(wrapper.find("input#audioFileInput").props().accept).toEqual(
+    "audio/*"
+  );
+  expect(wrapper.find(InputBase).props().placeholder).toEqual(
+    "Choose Audio File ..."
+  );
+});
+
+it("can be filtered for zip file types", () => {
+  const wrapper = mount(
+    <FileInput
+      id="zipFileInput"
+      fileType={FileType.ZIP}
+      callback={(file: File, fileURL: string) => {}}
+    />
+  );
+
+  expect(wrapper.find("input#zipFileInput").props().accept).toEqual(".zip");
+  expect(wrapper.find(InputBase).props().placeholder).toEqual(
+    "Choose Project Zip File ..."
+  );
 });
 
 const createFileList = (files: File[]): FileList => {
