7535776a839f0540162e792463d2c47712387dd5
tscz <tscz@users.noreply.github.com>
2020-01-06 23:56:29 +0900

    #19 Add enyzme for shallow tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,18 @@
+// Setup enzyme for shallow testing, see https://create-react-app.dev/docs/running-tests/
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
 // Ignore Tone.js browser compatibility warnings, see https://github.com/Tonejs/Tone.js/issues/439
 global.TONE_SILENCE_LOGGING = true;
+
+// Fail tests on warn messages
+global.console.warn = message => {
+  throw message;
+};
+
+// Fail tests on error messages
+global.console.error = message => {
+  throw message;
+};
