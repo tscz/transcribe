8b35b93a79e0eabec01121d4d2578a433165ae9b
tscz <tscz@users.noreply.github.com>
2020-01-10 18:10:38 +0900

    #12 Add debug config for storybooks

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,3 +1,8 @@
 import { configure } from "@storybook/react";
 
 configure(require.context('../src', true, /\.stories\.tsx$/), module);
+
+// Enable logging in storybook development mode
+if (process.env.NODE_ENV !== "production") {
+  localStorage.setItem("debug", "transcribe:*");
+}
