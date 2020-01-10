7eb0be3bf05963fa3341f4ae07113d4fd2739115
tscz <tscz@users.noreply.github.com>
2020-01-10 19:00:27 +0900

    #12 Comment storybook config

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,5 +1,6 @@
 import { configure } from "@storybook/react";
 
+// Only consider *.stories.tsx for storybook pages
 configure(require.context('../src', true, /\.stories\.tsx$/), module);
 
 // Enable logging in storybook development mode
