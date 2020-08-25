0ec7d16ba2810ac0da67c11b60c8d8cfd829d614
tscz <tscz@users.noreply.github.com>
2020-08-25 07:46:01 +0000

    Deploying to storybook from master 003411e8252e411f98a8b3fae5ebe6b4ff2da360

---
+++ precache-manifest.a4fe97eb4125da05be57b59a7ff352e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6ccc15cadea4c52a9a49a91ce3ad00",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.32614f2063d0d9e58e10.bundle.js"
+  },
+  {
+    "url": "runtime~main.32614f2063d0d9e58e10.bundle.js"
+  },
+  {
+    "url": "vendors~main.32614f2063d0d9e58e10.bundle.js"
+  },
+  {
+    "url": "vendors~main.32614f2063d0d9e58e10.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8661cd24da306020a4b2c038ff984440.js"
+  "precache-manifest.a4fe97eb4125da05be57b59a7ff352e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/transcribe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
