6436052287d9b842d0019d955b785693bc7a585c
tscz <tscz@users.noreply.github.com>
2021-01-02 19:31:30 +0100

    #192 Use Enzyme Adapter for React 17

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
 // Setup enzyme for shallow testing, see https://create-react-app.dev/docs/running-tests/
 import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 configure({ adapter: new Adapter() });
 
