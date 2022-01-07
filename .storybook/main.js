3dc14421d307972af5744e8d9f5e7e7e2f643442
tscz <tscz@users.noreply.github.com>
2022-01-08 00:35:17 +0100

    #15 use webpack 5 in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,17 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
+  core: {
+    builder: "webpack5",
+  },
+  stories: ["../src/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
   addons: [
-      '@storybook/preset-create-react-app',
-      '@storybook/addon-actions',
-      '@storybook/addon-knobs',
-      {
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-knobs",
+    {
       name: "@storybook/addon-docs",
       options: {
-        configureJSX: true
-      }
-    }
-    ]
+        configureJSX: true,
+      },
+    },
+  ],
 };
-
